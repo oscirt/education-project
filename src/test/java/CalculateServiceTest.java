b434db74713f74f0f2aa36f1f78dd5ebb40fbc85
oscirt <kadyrov.a.k@edu.mirea.ru>
2023-12-11 20:00:43 +0300

    Исправил CalculateService

--- src/test/java/CalculateServiceTest.java
+++ src/test/java/CalculateServiceTest.java
@@ -7,21 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculateServiceTest {
 
-    private final static CalculateService calculateService = new CalculateService();
-
     @Test
     public void calculateServiceCheck() {
         CalculatedSum sum;
 
-        sum = calculateService.calculate(new Product(10, 99.99, 0.75));
+        sum = CalculateService.calculate(new Product(10, 99.99, 0.75));
         assertEquals(sum.getSum().doubleValue(), 999.9);
         assertEquals(sum.getDiscountSum().doubleValue(), 992.4);
 
-        sum = calculateService.calculate(new Product(30, 9.99, 42.575));
+        sum = CalculateService.calculate(new Product(30, 9.99, 42.575));
         assertEquals(sum.getSum().doubleValue(), 299.7);
         assertEquals(sum.getDiscountSum().doubleValue(), 172.1);
 
-        sum = calculateService.calculate(new Product(50, 19.99, 59.1));
+        sum = CalculateService.calculate(new Product(50, 19.99, 59.1));
         assertEquals(sum.getSum().doubleValue(), 999.5);
         assertEquals(sum.getDiscountSum().doubleValue(), 408.8);
     }
