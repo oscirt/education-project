4127577a654991f2f6b5f1a8d505859d51ea6118
oscirt <kadyrov.a.k@edu.mirea.ru>
2024-02-29 17:07:22 +0300

    [HW-9] Update mock test

--- application/src/test/java/org/example/application/AnimalsRepositoryTest.java
+++ application/src/test/java/org/example/application/AnimalsRepositoryTest.java
@@ -5,9 +5,10 @@ import org.example.animalsstarter.repository.AnimalsRepositoryImpl;
 import org.example.animalsstarter.service.interfaces.CreateAnimalService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
@@ -20,8 +21,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
 @ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 public class AnimalsRepositoryTest {
 
     @Mock
